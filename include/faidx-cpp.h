89f0d5e3e86e4d6b6a15778ac0a6a240c8337402
GrantDaly <gdaly9000@gmail.com>
2022-02-25 11:25:02 -0600

    first code added

---
+++ include/faidx-cpp.h
@@ -0,0 +1,13 @@
+#ifndef FAIDX_H
+#define FAIDX_H
+
+#include <htslib/faidx.h>
+class FastaWrapper {
+ private:
+  //faidx_t* faidx_ptr = nullptr;
+  
+
+};
+
+
+#endif
---
+++ source/faidx-cpp.cpp
@@ -0,0 +1,4 @@
+#include "faidx-cpp.h"
+#include <iostream>
+
+//std::cout << "hello world" << std::endl;
