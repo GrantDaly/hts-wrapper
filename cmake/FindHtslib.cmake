88227139d740732c36824ac0eb06dd2d073393dc
GrantDaly <gdaly9000@gmail.com>
2025-09-29 09:52:38 -0500

    Updated CMake to be cleaner

--- cmake/FindHtslib.cmake
+++ cmake/FindHtslib.cmake
@@ -1,32 +1,40 @@
 find_package(PkgConfig)
 if(PKG_CONFIG_FOUND)
-  pkg_check_modules(Htslib REQUIRED htslib)
-endif()
+  # can add back QUIET flag eventually to allow lower logging
+  pkg_check_modules(PC_Htslib QUIET htslib)
 
- find_path(Htslib_INCLUDE_DIR
-   NAMES hts.h
-   HINTS ${Htslib_INCLUDE_DIRS}
-   PATH_SUFFIXES htslib
- )
- find_library(Htslib_LIBRARY
-   NAMES htslib hts
-   HINTS ${Htslib_LIBRARY_DIRS}
- )
+  find_path(Htslib_INCLUDE_DIR
+  NAMES hts.h
+  HINTS ${PC_Htslib_INCLUDE_DIRS}
+  PATH_SUFFIXES htslib
+)
 
-#set(Htslib_VERSION ${Htslib_VERSION})
+find_library(Htslib_LIBRARY
+  NAMES hts
+  HINTS ${PC_Htslib_LIBRARY_DIRS}
+)
 
-include(CMakePrintHelpers)
-cmake_print_variables(Htslib_LIBRARY)
-cmake_print_variables(Htslib_LIBRARY_DIRS)
-cmake_print_variables(Htslib_INCLUDE_DIR)
-cmake_print_variables(Htslib_INCLUDE_DIRS)
+set(Htslib_VERSION ${PC_Htslib_VERSION})
 
-if(Htslib_FOUND AND NOT TARGET hts::htslib)
-  message("Adding hts::htslib library target")
-  add_library(hts::htslib UNKNOWN IMPORTED)
-  set_target_properties(hts::htslib PROPERTIES
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Htslib
+  REQUIRED_VARS
+    Htslib_LIBRARY
+    Htslib_INCLUDE_DIR
+  VERSION_VAR Htslib_VERSION
+)
+
+if(Htslib_FOUND AND NOT TARGET Hts::Hts)
+  add_library(Hts::Hts UNKNOWN IMPORTED)
+  set_target_properties(Hts::Hts PROPERTIES
     IMPORTED_LOCATION "${Htslib_LIBRARY}"
-    INTERFACE_COMPILE_OPTIONS "${Htslib_CFLAGS_OTHER}"
-    INTERFACE_INCLUDE_DIRECTORIES "${Htslib_INCLUDE_DIR}"
+    INTERFACE_COMPILE_OPTIONS "${PC_Htslib_CFLAGS_OTHER}"
+    INTERFACE_INCLUDE_DIRECTORIES "${Htslib_INCLUDE_DIR}" #"should only contain information about the target itself, and not any of its dependencies."
   )
+
+  message("Include" ${Htslib_INCLUDE_DIR})
+  message("Lib" ${Htslib_LIBRARY})
+  message("I'face compile opt. " "${PC_Htslib_CFLAGS_OTHER}")
+  message("Version" ${Htslib_VERSION})
+endif()
 endif()
